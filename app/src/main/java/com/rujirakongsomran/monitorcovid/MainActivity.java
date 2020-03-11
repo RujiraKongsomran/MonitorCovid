5ef06d70ce0730bf6805a3d34b7df0adba819e09
Rujira Kongsomran <biwberry@outlook.com>
2020-03-11 14:00:20 +0700

    Call Service using Volley library

--- app/src/main/java/com/rujirakongsomran/monitorcovid/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/monitorcovid/MainActivity.java
@@ -4,11 +4,53 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initInstances();
+    }
+
+    private void initInstances() {
+        mQueue = Volley.newRequestQueue(this);
+        jsonParse();
+    }
+
+    private void jsonParse() {
+        String url = "http://cake88-001-site1.etempurl.com/covid19";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String s = "";
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                500000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        mQueue.add(request);
     }
 }
